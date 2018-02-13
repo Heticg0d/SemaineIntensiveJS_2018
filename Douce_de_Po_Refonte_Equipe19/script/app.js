9dad8fb4fd372cb216b102a3215ee1476bf33144
Heticg0d <maxime.cassagne@gmail.com>
2018-02-13 10:05:30 +0100

    repo
    repo

--- Douce_de_Po_Refonte_Equipe19/script/app.js
+++ Douce_de_Po_Refonte_Equipe19/script/app.js
@@ -4,7 +4,7 @@
  * @Last modified by:   Clem
  * @Last modified time: 2018-02-12T14:13:32+01:00
  */
-function myFunction()
+function myFunctionCoussin()
 {
 	var realInput=document.getElementById("baby_input").value /*nous allons faire de l'input un input traitable*/
 	let alph = "abcdefghijklmnopqrstuvwxyz"
@@ -25,17 +25,17 @@ function myFunction()
 				input+="c"
 		}
 	}
-	document.write(input)
 	/*nous allons imprimer le nom du bébé sur le coussin*/
-	for (let k=0; k<12; k++)
+	for (let k=0; k<8; k++)
 	{
 		
 	}
 	if (input=="b")
 	{
 		var img = document.createElement('img')
-		img.src = 'images/B_letter.png';
+		img.src = 'images/Z_letter50x50.png';
 		document.getElementById('coussin').appendChild(img);
+		/*img.*/
 	}
 	
 	/*document.getElementById("coussin").innerHTML="<img src='../images/A_letter.png' />";*/

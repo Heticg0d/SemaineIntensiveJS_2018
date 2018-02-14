190f549df81c5c1012cfefbb29478fed91c21e43
ClemPrt <clement.parot@hetic.net>
2018-02-14 16:40:54 +0100

    pagehome 2
    page home2

--- Douce_de_Po_Refonte_Equipe19/Clément/script/app.js
+++ Douce_de_Po_Refonte_Equipe19/Clément/script/app.js
@@ -2,7 +2,7 @@
  * @Author: Clem
  * @Date:   2018-02-12T14:13:32+01:00
  * @Last modified by:   Clem
- * @Last modified time: 2018-02-13T22:49:55+01:00
+ * @Last modified time: 2018-02-14T16:21:55+01:00
  */
 
 //carousel

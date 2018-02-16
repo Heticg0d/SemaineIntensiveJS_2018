aa6eb3129d869d1e4865864469375c1ec4a03438
ClemPrt <clement.parot@hetic.net>
2018-02-16 02:41:28 +0100

    Nuit blanche
    Nuit blanche

--- Douce_de_Po_Refonte_Equipe19/Clément/script/app.js
+++ Douce_de_Po_Refonte_Equipe19/Clément/script/app.js
@@ -2,7 +2,7 @@
  * @Author: Clem
  * @Date:   2018-02-12T14:13:32+01:00
  * @Last modified by:   Clem
- * @Last modified time: 2018-02-15T11:17:07+01:00
+ * @Last modified time: 2018-02-16T01:06:51+01:00
  */
 
 //carousel
@@ -20,3 +20,23 @@
      x[slideIndex-1].style.display = "block";
      setTimeout(carousel, 5000); // Change image every 2 seconds
  }
+
+ function hamburger_click()
+ {
+   var hamburgerElement = document.getElementById("hamburger");
+   var menuElement = document.getElementById("mdr");
+   var logo = document.getElementById("menu_logo_doucedepo");
+
+   hamburgerElement.style.display = "block";
+   menuElement.style.display = "block";
+   logo.style.display = "none";
+ }
+
+ function add() //ADDS +1 to the "number" in the HTML code each time "ajouter au panier" is clicked
+ {
+ 	var number = window.prompt("Choisir le nombre d'articles")
+ 	var previousNumber = document.getElementById("number").innerHTML
+ 	number = parseInt(number) + parseInt(previousNumber)
+ 	document.getElementById("number").innerHTML = number;
+ 	alert("Ajouté au panier !")
+}
